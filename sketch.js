ca8e6e0ae9a24a14f04cd84cb7b5c4fea9e11e33
flechsig <paul.flechsig@aol.de>
2023-01-09 07:48:54 +0100

    minors

--- sketch.js
+++ sketch.js
@@ -34,8 +34,10 @@ function setup() {
 	map2 = new MarchingSquaresMapGenerator(width*2, height*2, 15, true, 10);
 	// player = new Player({x: map.width/2 * map.scale, y:map.height/2 * map.scale})
 
-	player = new Player({x: map2.rows/2 * map2.rez, y:map2.cols/2 * map2.rez})
-	frameRate(60)
+	// player = new Player({x: map2.rows/2 * map2.rez, y:map2.cols/2 * map2.rez})
+	player = new Player({x: map2.rows/2 * map2.rez, y:-map2.cols/8 * map2.rez})
+	frameRate(120)
+	// createLoop({duration:3, gif:true, download: true})
 }
 
 function draw() {
@@ -58,8 +60,6 @@ function draw() {
 	if(webglOn) {
 		translate(-width / 2, -height / 2, 0);
 	}
-
-
 
 	map2.display();
 
